f0833c3468d7e142d9a3435aea50dfdba677b864
aditya <adityasingharyan007@gmail.com>
2024-11-25 10:23:42 +0530

    firstcommit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Review(122)</div>
+
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce  Website is an online platform that facilitate
+            Welcome to [ e-commerce  Website], your ultimate destination for diverse and stylish clothing for all genders. From chic dresses and tailored suits to comfortable casual wear and versatile unisex pieces, our collection has something for everyone. Explore the latest trends and timeless classics, all crafted with quality and affordability in mind. Whether shopping for yourself or the entire family, enjoy a seamless experience with easy navigation and detailed product information. Discover fashion without limits and elevate your wardrobe with [ e-commerce  Website] today!
+            </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
