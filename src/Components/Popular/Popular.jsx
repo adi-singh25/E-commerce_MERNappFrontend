c37366b3f77495461bb871ae33fe9f4c948004c2
karmyogiaditya <138021554+adi-singh25@users.noreply.github.com>
2025-05-12 12:23:38 +0530

    Update Popular.jsx

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -9,7 +9,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts]=useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    fetch('https://e-commerce-mernappbackend-1.onrender.com/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data));
   },[])
